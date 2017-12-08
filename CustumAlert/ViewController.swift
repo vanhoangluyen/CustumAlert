36023e418928a6687e08f0a42e7b9b9b86774a01
Hoang Van Luyen <vanhoangluyen@gmail.com>
2017-12-08 07:37:54 +0700

    default alert

--- CustumAlert/ViewController.swift
+++ CustumAlert/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPopoverPresentationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func clickSave(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Warning", message: "Message Warning", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action in
+            print("OK")
+        })
+        let yesAction = UIAlertAction(title: "Yes", style: .destructive, handler: {action in
+            print("YES")
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {action in
+            print("Cancel")
+        })
+        alert.addAction(okAction)
+        alert.addAction(yesAction)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
